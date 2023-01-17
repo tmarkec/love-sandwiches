ec8fdd4d6a8d4d9f2faf4ad103f24deab3371d8a
tmarkec <tmarkec@gmail.com>
2023-01-17 11:42:15 +0000

    adjust validation to check that all values are integers

--- run.py
+++ run.py
@@ -26,18 +26,21 @@ def get_sales_data():
     # removes comas from the strings
     validate_data(sales_data)
 
-    
+
 def validate_data(values):
     """ 
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
+
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
 
