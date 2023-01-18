7001830dff17946a80f5a8c322db64036973a684
tmarkec <tmarkec@gmail.com>
2023-01-18 11:12:20 +0000

    add calculation for stock data for next day

--- run.py
+++ run.py
@@ -115,7 +115,23 @@ def get_last_5_entries():
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
+    
     return columns
+
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...")
+
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / 5
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    return new_stock_data
 
 
 def main():
@@ -130,8 +146,11 @@ def main():
     new_surplus_date = calculate_surplus(sales_data)
     update_worksheet(new_surplus_date, 'surplus')
     sales_column = get_last_5_entries()
+    stock_data = calculate_stock_data(sales_column)
+    update_worksheet(stock_data, 'stock')
+    
 
-print("Welcome to Love Sandwiches Data Automation \n")
+print("Welcome to Love Sandwiches Data Automation")
 
 
 main()
